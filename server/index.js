6f3fd022220320680d497090cf33cce25a389839
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2020-11-23 18:42:28 +0200

    keycloak-react example

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const session = require("express-session");
 const Keycloak = require("keycloak-connect");
-var memoryStore = new session.MemoryStore();
-var keycloak = new Keycloak({ store: memoryStore });
+let memoryStore = new session.MemoryStore();
+let keycloak = new Keycloak({ store: memoryStore });
 
-// Configure session
+// Attach middleware
+// session
 app.use(
     session({
         secret: "mySecret",
@@ -15,32 +17,32 @@ app.use(
     })
 );
 
-// Attach middleware
+// cors
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// keycloak
 app.use(keycloak.middleware());
 
 // Attach route handler for home page
-app.get("/", (req, res, next) => {
-    res.json({ status: "Public page" });
+app.get("/anonymous", (req, res, next) => {
+    console.log("/anonymous");
+    res.json({ user: "Anonymous" });
 });
 
-app.get("/users", keycloak.protect("common_user"), function (req, res) {
+app.get("/user", keycloak.protect(), function (req, res) {
+    console.log("/user");
     res.send({
-        users: [
-            {
-                name: "Gosheto",
-                age: 31,
-                country: "Poland"
-            },
-            {
-                name: "Kateto",
-                age: 25,
-                country: "Bulgaria"
-            }
-        ]
+        user: "common_user"
     });
 });
 
 // Start server
-app.listen(9100, () => {
-    console.log("Server listening on port 9100!");
+app.listen(9000, () => {
+    console.log("Server listening on port 9000!");
 });
--- src/QueryAPI.js
+++ src/QueryAPI.js
@@ -1,54 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class APIResponse extends Component {
-  render() {
-    if(!this.props.response)
-      return (<div/>);
-    else
-      return ( <pre>{this.props.response}</pre> );
-  }
+    render() {
+        if (!this.props.response) return <div />;
+        else return <pre>{this.props.response}</pre>;
+    }
 }
 
 class QueryAPI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { response: null };
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = { response: null };
-  }
+    authorizationHeader() {
+        if (!this.props.keycloak) return {};
+        return {
+            headers: {
+                Authorization: "Bearer " + this.props.keycloak.token
+            }
+        };
+    }
 
-  authorizationHeader() {
-    if(!this.props.keycloak) return {};
-    return {
-      headers: {
-        "Authorization": "Bearer " + this.props.keycloak.token
-      }
+    handleClick = () => {
+        fetch("http://localhost:9000/user", this.authorizationHeader())
+            .then((response) => {
+                if (response.status === 200) return response.json();
+                else return { status: response.status, message: response.statusText };
+            })
+            .then((json) =>
+                this.setState((state, props) => ({
+                    response: JSON.stringify(json, null, 2)
+                }))
+            )
+            .catch((err) => {
+                this.setState((state, props) => ({ response: err.toString() }));
+            });
     };
-  }
 
-  handleClick = () => {
-    fetch('http://localhost:9100/users', this.authorizationHeader())
-      .then(response => {
-        if (response.status === 200)
-          return response.json();
-        else
-          return { status: response.status, message: response.statusText }
-      })
-      .then(json => this.setState((state, props) => ({
-        response: JSON.stringify(json, null, 2)
-      })))
-      .catch(err => {
-        this.setState((state, props) => ({ response: err.toString() }))
-      })
-  }
-
-  render() {
-    return (
-      <div className="QueryAPI">
-        <button onClick={this.handleClick}>Send API request</button>
-        <APIResponse response={this.state.response}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="QueryAPI">
+                <button onClick={this.handleClick}>Send API request</button>
+                <APIResponse response={this.state.response} />
+            </div>
+        );
+    }
 }
 
 export default QueryAPI;
