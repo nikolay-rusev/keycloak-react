4c41a69a7bf2c628bee7ce30a5d5e92e107a141b
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2020-11-23 15:35:24 +0200

    keycloak-react example

--- src/Logout.js
+++ src/Logout.js
@@ -3,17 +3,18 @@ import { withRouter } from 'react-router-dom'
 
 class Logout extends Component {
 
-    logout() {
-        this.props.history.push('/');
-        this.props.keycloak.logout();
-    }
+  logout() {
+    this.props.history.push('/')
+    this.props.keycloak.logout();
+  }
 
-    render() {
-        return (
-            <button onClick={ () => this.logout() }>
-                Logout
-            </button>
-        );
-    }
+  render() {
+    return (
+      <button onClick={ () => this.logout() }>
+        Logout
+      </button>
+    );
+  }
 }
+
 export default withRouter(Logout);
